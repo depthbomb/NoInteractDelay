85cf5cb683620339516650ea37f25bb2b2c84d5d
depthbomb <depthbomb@users.noreply.github.com>
2024-01-13 15:06:34 -0600

    refactor: remove unnecessary singleton pattern

--- NoInteractDelay/NoInteractDelayPlugin.cs
+++ NoInteractDelay/NoInteractDelayPlugin.cs
@@ -1,6 +1,5 @@
 ﻿using BepInEx;
 using HarmonyLib;
-using BepInEx.Logging;
 using NoInteractDelay.Patches;
 
 namespace NoInteractDelay
@@ -8,17 +7,10 @@ namespace NoInteractDelay
     [BepInPlugin(Constants.PluginGuid, Constants.Name, Constants.Version)]
     public class NoInteractDelayPlugin : BaseUnityPlugin
     {
-        public static NoInteractDelayPlugin Instance;
-
         private readonly Harmony _harmony = new Harmony(Constants.PluginGuid);
 
         private void Awake()
         {
-            if (Instance == null)
-            {
-                Instance = this;
-            }
-            
             GlobalShared.Logger.LogInfo($"Loaded {Constants.Name} v{Constants.Version}");
             
             _harmony.PatchAll(typeof(RemoveInteractionDelay));
